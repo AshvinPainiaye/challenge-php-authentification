bf9d1afe9ba0d0450167ca0840f8a3addb9518f4
AshFromReunion <ash-concept@hotmail.fr>
2016-05-23 21:02:08 +0400

    maj

--- mysql-crud/PDO.php
+++ mysql-crud/PDO.php
@@ -2,7 +2,7 @@
 
         try
 {
-    $bdd = new PDO('mysql:host=localhost;dbname=reunion_island;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=reunion_island;charset=utf8', 'root', 'ENTRER MOT DE PASSE');
 }
 
 catch (Exception $e)
