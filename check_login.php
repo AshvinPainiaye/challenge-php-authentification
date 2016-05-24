faf18f664cbe090a3cad5b3684a2c68191649b9f
AshFromReunion <ash-concept@hotmail.fr>
2016-05-24 14:02:45 +0400

    maj

--- check_login.php
+++ check_login.php
@@ -30,17 +30,17 @@ $req->closeCursor();
 		// dans ce cas, tout est ok, on peut démarrer notre session
 
 		session_start ();
-		// on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
+		// on enregistre les paramètres de notre visiteur comme variables de session ($username et $password) (notez bien que l'on utilise pas le $ pour enregistrer ces variables)
 		$_SESSION['username'] = $username;
 		$_SESSION['password'] = $password;
 
-		// on redirige notre visiteur vers une page de notre section membre
+		// on redirige notre membre vers la page read
 		header ('location: php-mysql-crud/read.php');
 	}
 	else {
 		// Le visiteur n'a pas été reconnu comme étant membre de notre site. On utilise alors un petit javascript lui signalant ce fait
 		echo '<body onLoad="alert(\'Membre non reconnu...\')">';
-		// puis on le redirige vers la page d'accueil
+		// puis on le redirige vers la page de login
 		echo '<meta http-equiv="refresh" content="0;URL=login.php">';
 	}
 }
